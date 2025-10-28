5f8f4a00caa4408f0e8f74eafc655fcc620540a9
Eray <109615405+ErayYuksek@users.noreply.github.com>
2025-10-28 21:10:33 +0300

    Time: 0 ms (100.00%) | Memory: 43.6 MB (69.30%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */public class Solution {
+    public ListNode DeleteDuplicates(ListNode head) {
+        if (head == null) return null;
+
+        ListNode current = head;
+
+        while (current != null && current.next != null)
+        {
+            if (current.val == current.next.val)
+            {
+                // tekrarı atla
+                current.next = current.next.next;
+            }
+            else
+            {
+                // ilerle
+                current = current.next;
+            }
+        }
+
+        return head; // <- eksik olan buydu
+    }
+}
