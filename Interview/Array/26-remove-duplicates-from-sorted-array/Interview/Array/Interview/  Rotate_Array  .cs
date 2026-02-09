119412bd67ee573acba7b82e8e88a285a95405dd
Eray <109615405+ErayYuksek@users.noreply.github.com>
2026-02-09 23:17:10 +0300

    Add files via upload

---
+++ Interview/Array/26-remove-duplicates-from-sorted-array/Interview/Array/Interview/  Rotate_Array  .cs
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        k = k % nums.Length;
+
+        Reverse(nums, 0, nums.Length - 1);
+        Reverse(nums, 0, k - 1);
+        Reverse(nums, k, nums.Length - 1);
+    }
+
+    private void Reverse(int[] nums, int left, int right)
+    {
+        while (left < right)
+        {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
